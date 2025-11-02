d6ad7ca59a5d4117b59dae06786d1d4e5febe2da
Micah Maurice Bassey <micahbassy2004@gmail.com>
2025-11-02 06:45:25 +0100

    W01 Home Page completed

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Display the current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display the document's last modified date
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
